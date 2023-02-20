402fb9cc9fbfc2e262506d99bbc0f066c36e4f2c
BBs Stuff <30535894+bbscode@users.noreply.github.com>
2023-02-20 20:39:33 +0200

    Update Client.java

--- Java-Testing/src/Client.java
+++ Java-Testing/src/Client.java
@@ -1,29 +1,41 @@
 package src;
+
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 import src.*;
 
 public class Client {
-    public static ObjectOutputStream out;
 
     public static void main(String[] args) throws IOException {
+
+        ObjectOutputStream out = null;
+        Scanner input = new Scanner(System.in);
+
         try (Socket s = new Socket("localhost", 6969)) {
             out = new ObjectOutputStream(s.getOutputStream());
-            Scanner input = new Scanner(System.in);
-      
-            out.writeObject(Message.sendMessage(input.nextLine()));
-            out.flush();
-            
-            input.close();
-            out.close();
-            s.close();
+            String message;
+            while ("exit".equalsIgnoreCase((message = input.nextLine()))) {
+                out.writeObject(message);
+                out.flush();
+            }
+
         } catch (IOException e) {
             // TODO: handle exception
             e.printStackTrace();
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            input.close();
+
         }
 
     }
-  
 
 }
